b2dd99a014e5e647fd4ba5ed93e36ccd3f1eb342
bema97 <ibermet@gmail.com>
2019-06-17 18:43:44 +0600

    homework

---
+++ ch2task_19.py
@@ -0,0 +1,10 @@
+my_weight=50
+
+moon_weight=my_weight*0.165
+during_15_years=15*0.165
+resoult=moon_weight+during_15_years
+
+
+while moon_weight<=resoult:
+    print (moon_weight)
+    moon_weight +=0.165
